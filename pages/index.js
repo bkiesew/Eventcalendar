4df16eec81d5e224760340176c88c055bfc607fb
bkiesew <bkiesew@gmail.com>
2024-12-13 10:46:12 -0800

    Create index.js
    Add main page with calculator

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PackageCalculator from '../components/PackageCalculator';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <div className="max-w-7xl mx-auto px-4">
+        <PackageCalculator />
+      </div>
+    </div>
+  );
+}
